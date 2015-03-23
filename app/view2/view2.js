141ad47b60cc7b3fc6ba1b3404de1377bbf09965
kvvn <kvvn@i.kiev.ua>
2015-03-23 18:31:39 +0200

    avia ticket search form

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -36,9 +36,9 @@ angular.module('myApp.view2', ['ngRoute'])
         };
 
         //Search for avia tickets request
-        $scope.searchTicket = function(city, date){
+        $scope.searchTicket = function(city, country, date){
 
-            $http.get('http://localhost:4567/tickets?city=' + city + '&date=' + date).
+            $http.get('http://localhost:4567/tickets?city=' + city + '&country=' + country + '&date=' + date).
                 success(function (data, status, headers, config) {
                     //console.log(data);
 
@@ -52,5 +52,12 @@ angular.module('myApp.view2', ['ngRoute'])
 * current JSON example of event from seatgeek
 *
 * "{"links":[],"id":2471323,"stats":{"listing_count":0,"average_price":null,"lowest_price":null,"highest_price":null},"title":"Acdc","announce_date":"2015-01-10T00:00:00","score":0.53177,"date_tbd":false,"type":"concert","datetime_local":"2015-06-28T20:00:00","visible_until_utc":"2015-06-28T23:00:00","time_tbd":false,"taxonomies":[{"parent_id":null,"id":2000000,"name":"concert"}],"performers":[{"stats":{"event_count":2},"name":"Acdc","short_name":"Acdc","url":"https://seatgeek.com/acdc-tickets","type":"band","image":"https://chairnerd.global.ssl.fastly.net/images/performers-landscape/acdc-74e564/145510/huge.jpg","home_venue_id":null,"primary":true,"score":0,"images":{"huge":"https://chairnerd.global.ssl.fastly.net/images/performers-landscape/acdc-74e564/145510/huge.jpg"},"slug":"acdc","taxonomies":[{"parent_id":null,"id":2000000,"name":"concert"}],"has_upcoming_events":true,"id":145510}],"url":"https://seatgeek.com/acdc-tickets/glasgow-hampden-park-2015-06-28-8-pm/concert/2471323/","created_at":"2015-01-10T00:00:00","venue":{"city":"Glasgow","name":"Hampden Park","extended_address":"Glasgow, UK","url":"https://seatgeek.com/venues/hampden-park/tickets/","country":"UK","display_location":"Glasgow, UK","links":[],"slug":"hampden-park","state":"Glg","score":0.60772,"postal_code":"G42 9BA","location":{"lat":55.8259,"lon":-4.252},"address":"","timezone":"Europe/London","id":37850},"short_title":"Acdc","datetime_utc":"2015-06-28T19:00:00","datetime_tbd":false}"
+*
+* */
+
+/*
+*  current JSON example of AviaSales response
+*
+*  {"success": true, "data": {"2015-05-12":{"origin":"IEV","destination":"MUC","price":13256,"transfers":1,"airline":"KL","flight_number":1386,"departure_at":"2015-05-12T14:00:00Z","return_at":"2015-05-15T14:05:00Z","expires_at":"2015-03-26T09:27:20Z"},"2015-05-25":{"origin":"IEV","destination":"MUC","price":13435,"transfers":1,"airline":"KL","flight_number":1386,"departure_at":"2015-05-25T14:00:00Z","return_at":"2015-06-01T20:30:00Z","expires_at":"2015-03-26T14:10:18Z"}}}
 *
 * */
