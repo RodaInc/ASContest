8b339eb3e991f89254e00218c11cb7e8fd776fe4
kvvn <kvvn@i.kiev.ua>
2015-03-25 17:37:18 +0200

    wide filter form

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,7 +9,7 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
       $scope.searchText = '';
 
       $scope.fulltextsearch = function () {
